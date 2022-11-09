4c6f5079ac228fc07188e59b9c47758bf862e1b0
ambsenyestudi <adrifre7@hotmail.com>
2022-11-09 19:10:42 +0100

    Project scafolding

---
+++ src/domain/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace SudokuSolver.Domain;
+public class Class1
+{
+
+}
---
+++ tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace SudokuSolver.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
